ad8aed06fd9da11c0bdc4ec0be2c5335101a1b32
nelly476 <nellybulatova12@gmail.com>
2022-11-15 13:01:59 +0300

    add styling

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
   return (
     <div className="App">
       <Navbar />
-      {cardElements}
+      <div className="content">{cardElements}</div>
     </div>
   );
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,14 +4,20 @@ function Card(props) {
       <img src={props.item.imageUrl} alt="Location-image" />
 
       <div className="info--section">
-        <span>
-          <p className="card--location">{props.item.location}</p>
+        <span className="location--section">
+          <span className="location--name">
+            <i class="fa-solid fa-location-dot"></i>
+            <p className="card--location">
+              {props.item.location.toUpperCase()}
+            </p>
+          </span>
+
           <a href={props.item.googleMapsUrl}>View on Google Maps</a>
         </span>
 
         <h1>{props.item.title}</h1>
 
-        <p>
+        <p className="date--info">
           <strong>
             {props.item.startDate} - {props.item.endDate}
           </strong>
